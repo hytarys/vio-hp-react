a0acfcfaa070c4a1e42cbc13665dbf5a84649021
hytarys <68804163+hytarys@users.noreply.github.com>
2021-06-12 23:36:14 +0900

    fix: 軽微修正

--- vio-hp/static.config.js
+++ vio-hp/static.config.js
@@ -28,7 +28,7 @@ export default {
   },
   plugins: [
     'react-static-plugin-typescript',
-    'react-static-plugin-sass'
+    'react-static-plugin-sass',
     [
       require.resolve('react-static-plugin-source-filesystem'),
       {
