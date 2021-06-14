a446188aeded989a6bd88ba158f92d00499dd2c7
hytarys <68804163+hytarys@users.noreply.github.com>
2021-06-15 02:02:22 +0900

    feat: sliderの実装

--- vio-hp/src/components/Slider.tsx
+++ vio-hp/src/components/Slider.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../html/css/style.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.scss';
+import 'swiper/swiper-bundle.css';
+import SwiperCore, { Autoplay, EffectFade } from 'swiper';
+
+SwiperCore.use([Autoplay, EffectFade]);
 
 const slideLogo1 = require('../html/img/IMG_4397.jpg')
 const slideLogo2 = require('../html/img/IMG_4440.jpg')
@@ -10,7 +13,14 @@ const slideLogo4 = require('../html/img/IMG_4465.jpg')
 
 const Slider: React.FC = () => {
   return <Swiper
-  className="p-slider"
+  tag="section"
+  wrapperTag="ul"
+  autoplay={{delay: 10000}}
+  onInit={(swiper :string) => console.log('Swiper initialized!', swiper)}
+  onReachEnd={() => console.log('Swiper end reached')}
+  loop={true}
+  effect = "fade"
+  speed = {2000}
   >
   <SwiperSlide><img src={slideLogo1} alt=""/></SwiperSlide>
   <SwiperSlide><img src={slideLogo2} alt="" /></SwiperSlide>
