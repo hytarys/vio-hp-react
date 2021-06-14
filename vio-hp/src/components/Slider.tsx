1b179cd4feb67776cfff0783053a79f6c318aec1
hytarys <68804163+hytarys@users.noreply.github.com>
2021-06-15 00:19:55 +0900

    feat: swiperの追加

--- vio-hp/src/components/Slider.tsx
+++ vio-hp/src/components/Slider.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../html/css/style.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+
+const slideLogo1 = require('../html/img/IMG_4397.jpg')
+const slideLogo2 = require('../html/img/IMG_4440.jpg')
+const slideLogo3 = require('../html/img/IMG_4452.jpg')
+const slideLogo4 = require('../html/img/IMG_4465.jpg')
 
 const Slider: React.FC = () => {
-  return <div className="p-slider">
-    aaaa
-  </div>
+  return <Swiper
+  className="p-slider"
+  >
+  <SwiperSlide><img src={slideLogo1} alt=""/></SwiperSlide>
+  <SwiperSlide><img src={slideLogo2} alt="" /></SwiperSlide>
+  <SwiperSlide><img src={slideLogo3} alt="" /></SwiperSlide>
+  <SwiperSlide><img src={slideLogo4} alt="" /></SwiperSlide>
+</Swiper>
 }
 export default Slider
