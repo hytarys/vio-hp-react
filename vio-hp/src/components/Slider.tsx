f39e1f224f030698b47e42d7ff3dc61ad7787df7
hytarys <68804163+hytarys@users.noreply.github.com>
2021-06-17 01:18:33 +0900

    feat: トップスライダーの作成

--- vio-hp/src/components/Slider.tsx
+++ vio-hp/src/components/Slider.tsx
@@ -6,26 +6,24 @@ import SwiperCore, { Autoplay, EffectFade } from 'swiper';
 
 SwiperCore.use([Autoplay, EffectFade]);
 
-const slideLogo1 = require('../html/img/IMG_4397.jpg')
-const slideLogo2 = require('../html/img/IMG_4440.jpg')
-const slideLogo3 = require('../html/img/IMG_4452.jpg')
-const slideLogo4 = require('../html/img/IMG_4465.jpg')
+const slideLogo1 = require('../html/img/slider-top1.png')
+const slideLogo2 = require('../html/img/slider-top2.png')
+const slideLogo3 = require('../html/img/slider-top3.png')
 
 const Slider: React.FC = () => {
   return <Swiper
   tag="section"
   wrapperTag="ul"
-  autoplay={{delay: 10000}}
-  onInit={(swiper :string) => console.log('Swiper initialized!', swiper)}
-  onReachEnd={() => console.log('Swiper end reached')}
-  loop={true}
+  autoplay={{stopOnLastSlide: true, delay: 1000}}
   effect = "fade"
-  speed = {2000}
+  speed = {3000}
+  allowTouchMove = {false}
+  className = 'p-slider-contents'
   >
+  <SwiperSlide></SwiperSlide>
   <SwiperSlide><img src={slideLogo1} alt=""/></SwiperSlide>
   <SwiperSlide><img src={slideLogo2} alt="" /></SwiperSlide>
   <SwiperSlide><img src={slideLogo3} alt="" /></SwiperSlide>
-  <SwiperSlide><img src={slideLogo4} alt="" /></SwiperSlide>
 </Swiper>
 }
 export default Slider
