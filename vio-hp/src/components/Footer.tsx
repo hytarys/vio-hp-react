f4497e4debbe427067209b7b15ff0c1257847551
hytarys <68804163+hytarys@users.noreply.github.com>
2021-07-02 01:33:23 +0900

    feat: footerの作成

--- vio-hp/src/components/Footer.tsx
+++ vio-hp/src/components/Footer.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import { Link } from "@reach/router";
 import '../html/css/style.css'
 
+const twitterImg = require('../html/img/twitter.png')
+const instaImg = require('../html/img/insta.png')
+
 const Footer: React.FC = () => {
   return (
-    <div className='p-footer-pc'>
-      <div className='p-footer-pc-menu'>
-        <Link to='#' className='p-footer-pc-menu__single'>Profile</Link>
-        <Link to='#' className='p-footer-pc-menu__single'>Lesson</Link>
-        <Link to='#' className='p-footer-pc-menu__single'>News</Link>
-        <Link to='#' className='p-footer-pc-menu__single'>Contact</Link>
+    <footer className='p-footer_bg'>
+      <div className='p-footer-pc u-flex u-flex_jc_sb u-flex_aa_center'>
+        <div className='p-footer-pc-logo'>
+          <div className='hp-logo p-footer-pc-logo__hp u-mb_lg u-ml_md'>HINAKO FUCHIINO</div>
+          <div className='p-footer-pc-logo-sns u-flex u-flex_aa_center u-flex_jc_sa'>
+            <div><img src={twitterImg} alt="" /></div>
+            <div><img src={instaImg} alt="" /></div>
+          </div>
+        </div>
+        <div className='p-footer-pc-menu p-footer-pc__menu'>
+          <Link to='#' className='p-footer-pc-menu__single'>Profile</Link>
+          <Link to='#' className='p-footer-pc-menu__single'>Lesson</Link>
+          <Link to='#' className='p-footer-pc-menu__single'>News</Link>
+          <Link to='#' className='p-footer-pc-menu__single'>Contact</Link>
+        </div>
       </div>
-    </div>
+      <div className="u-text_center u-mb_md"><span className="p-footer_copy">©️HinakoFuchino.co</span></div>
+    </footer>
   )
 }
 export default Footer
